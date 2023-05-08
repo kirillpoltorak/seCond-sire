e3638680649c3fe5cf55e3de68bb24c6f7de0418
kirillpoltorak <108293485+kirillpoltorak@users.noreply.github.com>
2023-05-08 19:13:48 +0300

    new site

---
+++ js/05.04.js
@@ -0,0 +1,12 @@
+var time = 9;
+console.log(time);
+
+var movies = [
+		{ name : "cars", "release date" : 2006, "duration" : "1h 57m", "director" : "John Lasseter" },
+		{ name : "John Wick", "release date" : 2014, "duration" : "1h 41m", "director" : "Chad Stahelski" },
+		{ name : "Death Race", "release date" : 2008, "duration" : "1h 51m", "director" : "Roel Raine, Paul Willem Scott Anderson, Don Michael Paul" }
+];
+
+console.log(movies);
+
+console.log(movies[1]["name"]);
